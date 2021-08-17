99b6b144ee7be8aa54668f77872ca53a3d97f1e8
Tilanka_Mihingu <tilankamihingu2018@gmail.com>
2021-08-17 11:40:43 +0530

    home modify

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Home.css';
-import first from '../../images/first.png';
+import first from '../../images/banner.png';
 
 const Home = () => {
     return (
@@ -9,7 +9,8 @@ const Home = () => {
                <div className="first-left">
                    <div className="first-left-details">
                         <h1>Miss Kottu</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt libero, dolorum at deleniti molestiae accusamus quasi quas dolorem nobis quod ipsa sint perspiciatis! Pariatur laudantium cupiditate qui quibusdam ducimus. Accusantium!</p>
+                        <button className="first-btn">Watch Items</button>
                    </div>
                    <div className="first-left-image">
                        <img src={first} alt="" />
